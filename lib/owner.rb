a06000df7f29234393796b548048efc34b633df7
Jesse Griffin <jesse.griffin@gmail.com>
2020-02-23 03:25:22 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -56,6 +56,6 @@ class Owner
   end 
   
   def list_pets 
-    "I have #{@owner.dogs.count} dog(s), and #{owner.cats.count} cat(s)."
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
   end 
 end
